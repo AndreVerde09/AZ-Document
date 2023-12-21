4c5e49e60ae58c43eb77a22fe0d67e8de0e05504
salvatorenarducci <salvatore.narducci@fides.it>
2023-12-21 15:00:05 +0100

    Some fixes

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var fs = require("fs");
 var html_to_pdf = require("html-pdf-node");
 
 var htmlRead = fs.readFileSync(
-  "02_-_Cribis_Approfondito_Snc/index.html",
+  "Cribis_Approfondito_Accomandita/index.html",
   "utf8"
 );
 var base64ImageHeader = fs.readFileSync("./logo.png", "base64");
 var base64ImageFooter = fs.readFileSync("./Rectangle.png", "base64");
 
-const header = `<img src="data:image/png;base64,${base64ImageHeader}" class="imageHeader" style="padding-left: 45px; padding-bottom: 140px">`;
+const header = `<img src="data:image/png;base64,${base64ImageHeader}" class="imageHeader" style="padding-left: 45px;">`;
 
 const cssFooter = `<style> .container {display: flex; justify-content: end; align-items: flex-end; width: 100%; padding-right: 5px; margin-top: 140px} .innerContainer {display: flex; justify-content: end; align-items: end; width: fit-content; gap: 3px} .boxNumber {width: 14px; height: 14px; background: #023671; -webkit-print-color-adjust: exact; display: flex; justify-content: center; align-items: center; border-radius: 50%; padding: 5px; margin-bottom: 8px} .boxNumber > h1 {font-size: 8px;color: #fff; } </style>`;
 
@@ -30,8 +30,8 @@ let options = {
   headerTemplate: header,
   footerTemplate: footer,
   margin: {
-    top: "200px",
-    bottom: "100px",
+    top: "150px",
+    bottom: "150px",
     right: "30px",
     left: "30px",
   },
@@ -41,5 +41,5 @@ let options = {
 let file = { content: htmlRead };
 
 html_to_pdf.generatePdf(file, options).then((pdfBuffer) => {
-  fs.writeFileSync("02_-_Cribis_Approfondito_Snc.pdf", pdfBuffer);
+  fs.writeFileSync("Cribis_Approfondito_Accomandita.pdf", pdfBuffer);
 });
